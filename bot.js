83cbbb1f4a885d4405b011605db53d1a57301a29
Quentius <44971401+Quentius@users.noreply.github.com>
2019-05-27 09:11:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -176,6 +176,16 @@ bot.on('message', message=>{
                 if(!message.member.roles.find(r => r.name === "Guest")) return message.channel.send("You do not have permissions")
                     message.channel.sendMessage('In maintenance')
                     break;
+            case 'online':
+                if(!message.member.roles.find(r => r.name === "Guest")) return message.channel.send("You do not have permissions")
+                    let totalSeconds = (client.uptime / 1000);
+                    let days = Math.floor(totalSeconds / 86400);
+                    let hours = Math.floor(totalSeconds / 3600);
+                    totalSeconds %= 3600;
+                    let minutes = Math.floor(totalSeconds / 60);
+                    let seconds = totalSeconds % 60;
+                    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+                    message.channel.sendMessaage(uptime);
         }
         switch(args[0]){
             case 'esl1':
