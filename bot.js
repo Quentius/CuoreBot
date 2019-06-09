d3b9151299030b391df1ba84b82eede2546ed620
Quentius <44971401+Quentius@users.noreply.github.com>
2019-06-09 18:43:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,9 @@
-    
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 client.on('message', message => {
     if (message.content === 'c/teams') {
@@ -198,7 +201,7 @@ client.on('message', message => {
                     break;
     }
         
-})
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
