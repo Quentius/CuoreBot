2b251cc570dfb51fff638deec0e73eedddd28343
Quentius <44971401+Quentius@users.noreply.github.com>
2019-06-09 13:52:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 client.on('message', message => {
     if (message.content === 'teams') {
+        message.reply('pong');
     	if(!message.member.roles.find(r => r.name === "Moderator")) return message.channel.send("You do not have permissions")
                     const nlg = new  Discord.RichEmbed()
                     .setTitle('Team name: No Limits Gaming')
